3e61c53621a7af28c3a1aded60c2ab9acf8b7a42
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-04 00:18:20 +0000

    Completed typescript-primitives-and-variables.

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const fullName = 'Ryan Garcia';
+const isCool = true;
+const totalPets = 0;
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+const empty = null;
+console.log('value of empty:', empty);
+console.log('typeof empty:', typeof empty);
+const nothing = undefined;
+console.log('value of nothing:', nothing);
+console.log('typeof nothing:', typeof nothing);
