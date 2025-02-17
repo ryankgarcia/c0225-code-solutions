7150f13f9011ec823659b5c20d550895201eec21
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-17 23:19:19 +0000

    in progress.

---
+++ dom-events/buttons.js
@@ -0,0 +1,10 @@
+"use strict";
+function handleClick(event) {
+    console.log('button clicked');
+    console.log('event:', event);
+    console.log('event.target:', event.target);
+}
+const $clickButton = document.querySelector('.click-button');
+if (!$clickButton)
+    throw new Error('The $clickButton query failed');
+$clickButton.addEventListener('click', handleClick);
