2fa9b65738d60e5fd2dd21e6aff2227629fa0050
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-18 03:12:53 +0000

    Completed dom-querying.

--- dom-querying/main.ts
+++ dom-querying/main.ts
@@ -3,3 +3,22 @@ console.log('hello, world');
 const $heading = document.querySelector('h1');
 
 console.log('heading', $heading);
+console.dir('heading', $heading);
+
+const $id = document.querySelector('#explanation');
+
+console.log('id', $id);
+console.dir('id', $id);
+
+const $hint = document.querySelector('.hint');
+
+console.log('hint', $hint);
+console.dir('hint', $hint);
+
+const $pVariable = document.querySelectorAll('p');
+
+console.log('p', $pVariable);
+
+const $exampleLink = document.querySelectorAll('.example-link');
+
+console.log('example link', $exampleLink);
