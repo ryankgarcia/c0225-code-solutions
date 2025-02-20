5d245d61fbd0ecbe8525c00d49529c6c3fc09a87
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-20 21:40:45 +0000

    in progress typescript-forms.

--- typescript-forms/form-events.ts
+++ typescript-forms/form-events.ts
@@ -2,7 +2,8 @@ const $nameInput = document.querySelector('input');
 
 if (!$nameInput) throw new Error('$input does not exist');
 
-// write the below as function with word function so it can be reusable //
+// write the below arrow function as function with word function so it can be reusable //
+// we typically only want to write arrow functions as call back functions for readability purposes //
 
 $nameInput.addEventListener('focus', (event: Event): void => {
   console.log('focus event was fired');
@@ -10,4 +11,5 @@ $nameInput.addEventListener('focus', (event: Event): void => {
   console.log('event.target.name', eventTarget.name);
 });
 
-$input.addEventListener('blur', (event: Event): void => {});
+$nameInput.addEventListener('blur');
+$emailInput.addEventListener('blur');
