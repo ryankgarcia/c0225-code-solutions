53fadbda2b5dca92f1e38704f597f357b8db694c
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-21 22:44:44 +0000

    in progress.

---
+++ typescript-view-swapping/main.js
@@ -0,0 +1,10 @@
+"use strict";
+const $tabContainer = document.querySelector('.tab-container');
+const $tabs = document.querySelectorAll('.tab');
+const $views = document.querySelectorAll('.view');
+if (!$tabContainer)
+    throw new Error('$tabContainer query failed');
+// left off here in the code sequence //
+// $tabContainer.addEventListener('click', (event: Event) => {
+//   // const $eventTarget = event.target as HTMLDivElement; //left off here.
+// };
