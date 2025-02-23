07a755700a8a67c3cbb2bfae8edae5b7b46f4580
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-23 19:43:14 +0000

    in progress.

--- typescript-functions-practice/get-element-at-index.js
+++ typescript-functions-practice/get-element-at-index.js
@@ -1,3 +1,22 @@
 "use strict";
 /* exported getElementAtIndex */
-// comeback to this
+function getElementAtIndex(array, index) {
+    if (index < 0 || array.length < 0) {
+        return 'index value is invalid';
+    }
+    else {
+        return array[index];
+    }
+}
+console.log('array string output index[3]:', getElementAtIndex(['cat', 'dog', 'mouse', 'horse'], 3));
+console.log('array boolean output index[3]:', getElementAtIndex([true, false, false, false], 3));
+console.log('array number output index[3]:', getElementAtIndex([88, 99, 100, 2], 3));
+// NOTES about this function...
+// this function's conditional `if` statement starts false. and if that condition is false it returns
+// the string 'index value is invalid. this accounts for negative index numbers AND
+// index values that are larger than the length of the array
+// the cool thing about this `if` statement is that it checks two expressions using the logical OR operator
+// `else` it returns the array at index value, which will be passed in as an argument when calling the function
+// in the function call, each array is of an unknown data type, because it could be a string array, boolean, number
+// it doesn't specify, because we won't known UNTIL an array gets passed in as an argument.
+// which is the reason why the return type of our function will be unknown.
--- typescript-functions-practice/get-first-character.js
+++ typescript-functions-practice/get-first-character.js
@@ -1,2 +1,10 @@
 "use strict";
 /* exported getFirstCharacter */
+function getFirstCharacter(string) {
+    if (string.length >= 0) {
+        return string[0];
+    }
+}
+console.log('get first Character:', getFirstCharacter('Gary'));
+console.log('get first Character:', getFirstCharacter('Red'));
+console.log('get first Character:', getFirstCharacter('079'));
