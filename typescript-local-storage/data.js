039db1f8a92d720c0cef19857d2bcf8b62dccdec
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-26 19:03:21 +0000

    Completed typescript-local-storage.

--- typescript-local-storage/data.js
+++ typescript-local-storage/data.js
@@ -1,12 +1,10 @@
 "use strict";
-const todos = [];
+const todos = readTodos();
 function writeTodos() {
-    debugger;
     const todosJSON = JSON.stringify(todos);
     localStorage.setItem('todos-storage', todosJSON);
 }
 function readTodos() {
-    debugger;
     const todosStorage = localStorage.getItem('todos-storage');
     if (todosStorage) {
         const json = JSON.parse(todosStorage);
