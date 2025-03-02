1abbc28d048cec874de8d238f8148d461af30540
Ryan Garcia <ryankgarcia22@gmail.com>
2025-03-02 08:33:30 +0000

    in progress.

--- typescript-loops-practice/find-index.ts
+++ typescript-loops-practice/find-index.ts
@@ -1,1 +1,12 @@
 /* exported findIndex */
+
+function findIndex(array: number[], value: number): number {
+  const negativeVar: number = -1;
+  for (const num of array) {
+    if (value === num) {
+      return num;
+    } else {
+    }
+    return negativeVar;
+  }
+}
