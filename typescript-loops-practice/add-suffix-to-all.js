0acb37eae42cd9d5267a588ab694b11e031183db
Ryan Garcia <ryankgarcia22@gmail.com>
2025-03-02 05:51:42 +0000

    in progress.

--- typescript-loops-practice/add-suffix-to-all.js
+++ typescript-loops-practice/add-suffix-to-all.js
@@ -1,2 +1,11 @@
 "use strict";
 /* exported addSuffixToAll */
+function addSuffixToAll(words, suffix) {
+    const newStringArray = [];
+    for (const word of words) {
+        newStringArray.push(word + suffix);
+    }
+    return newStringArray;
+}
+console.log(addSuffixToAll(['walk', 'talk', 'go', 'tread', 'fall'], 'ing'));
+console.log(addSuffixToAll(['start', 'end', 'walk', 'need', 'fail'], 'ed'));
--- typescript-loops-practice/get-student-names.ts
+++ typescript-loops-practice/get-student-names.ts
@@ -1,1 +1,24 @@
 /* exported getStudentNames */
+
+interface Students {
+  name: string;
+}
+
+const students: Students[] = [
+  { name: 'Fred' },
+  { name: 'Kendal' },
+  { name: 'Vanessa' },
+  { name: 'Harry' },
+  { name: 'Will' },
+  { name: 'Jared' },
+];
+
+function getStudentNames(students: Students[]): string[] {
+  const studentArray: string[] = [];
+  for (const student of students) {
+    studentArray.push(student.name);
+  }
+  return studentArray;
+}
+
+console.log(getStudentNames(students));
