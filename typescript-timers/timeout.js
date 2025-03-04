9b779da3ad77ae0537ff0ba49d5cac78ed21fc78
Ryan Garcia <ryankgarcia22@gmail.com>
2025-03-04 03:47:25 +0000

    Completed typescript-timers.

--- typescript-timers/timeout.js
+++ typescript-timers/timeout.js
@@ -1,1 +1,8 @@
 "use strict";
+const $h1 = document.querySelector('.message');
+function showDisplay() {
+    if (!$h1)
+        throw new Error('$h1 query failed');
+    $h1.textContent = 'Hello There :)';
+}
+setTimeout(showDisplay, 2000);
