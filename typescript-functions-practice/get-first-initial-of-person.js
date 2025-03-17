e2b5fc3aa3330196824ffc96e43c18ba1fb8307d
Ryan Garcia <ryankgarcia22@gmail.com>
2025-03-17 04:58:19 +0000

    in progress.

--- typescript-functions-practice/get-first-initial-of-person.js
+++ typescript-functions-practice/get-first-initial-of-person.js
@@ -1,13 +1,28 @@
 "use strict";
 /* exported getFirstInitialOfPerson */
-const fullName = {
-    firstName: 'Ryan',
-    lastName: 'Garcia',
-};
-function getFirstInitialOfPerson(person) {
-    fullName;
-    if (person.firstName.length >= 0) {
-        return fullName.firstName[0];
+// interface Person {
+//   firstName?: string;
+//   lastName?: string;
+// }
+// const person: Person = {
+//   firstName: 'Jose',
+//   lastName: 'Jarim',
+// }
+function getFirstInitialOfPerson(firstName, lastName) {
+    const person = firstName + lastName;
+    const firstInitial = person[firstName];
+    if (typeof firstName === 'string' && firstName[length] > 0) {
+        return firstName[0];
     }
+    return firstInitial;
 }
-console.log('getFirstInitial:', getFirstInitialOfPerson('Joseph Jahill'));
+// function getFirstInitialOfPerson(obj: T, key: keyof T): string | undefined {
+//   const value = obj[key];
+//   if (typeof value === 'string' && value.length > 0) {
+//     return value[0];
+//   }
+//   return undefined;
+// }
+const person = { name: 'alice', age: 25 };
+console.log(getFirstInitialOfPerson(person, 'name'));
+console.log('getFirstInitial:', getFirstInitialOfPerson('Jose', 'Casillas'));
--- typescript-functions-practice/get-first-initial-of-person.ts
+++ typescript-functions-practice/get-first-initial-of-person.ts
@@ -1,20 +1,33 @@
 /* exported getFirstInitialOfPerson */
 
-interface Person {
-  firstName: string;
-  lastName: string;
-}
+// interface Person {
+//   firstName?: string;
+//   lastName?: string;
+// }
 
-const fullName: Person = {
-  firstName: 'Ryan',
-  lastName: 'Garcia',
-};
+// const person: Person = {
+//   firstName: 'Jose',
+//   lastName: 'Jarim',
+// }
 
-function getFirstInitialOfPerson(person: Person): string {
-  fullName;
-  if (person.firstName.length >= 0) {
-    return fullName.firstName[0];
+function getFirstInitialOfPerson(firstName: Person, lastName: Person): string {
+  const person: Person = firstName + lastName;
+  const firstInitial = person[firstName];
+  if (typeof firstName === 'string' && firstName[length] > 0) {
+    return firstName[0];
   }
+  return firstInitial;
 }
 
-console.log('getFirstInitial:', getFirstInitialOfPerson('Joseph Jahill'));
+// function getFirstInitialOfPerson(obj: T, key: keyof T): string | undefined {
+//   const value = obj[key];
+//   if (typeof value === 'string' && value.length > 0) {
+//     return value[0];
+//   }
+//   return undefined;
+// }
+
+const person = { name: 'alice', age: 25 };
+console.log(getFirstInitialOfPerson(person, 'name'));
+
+console.log('getFirstInitial:', getFirstInitialOfPerson('Jose', 'Casillas'));
