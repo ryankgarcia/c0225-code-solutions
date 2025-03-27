a284d29fbb0ec90b6d8edacb74b6b8f73b7b105d
Ryan Garcia <ryankgarcia22@gmail.com>
2025-03-27 06:08:59 +0000

    in progress.

---
+++ react-lists/my-app/src/App.tsx
@@ -0,0 +1,24 @@
+// import './App.css';
+// import { PokemonList } from './PokemonList';
+
+// export type Pokemon = {
+//   number: string;
+//   name: string;
+// }
+
+// const pokedex: Pokemon[] = [
+//   { number: '001', name: 'Bulbasaur' },
+//   { number: '004', name: 'Charmander' },
+//   { number: '007', name: 'Squirtle' },
+//   { number: '025', name: 'Pikachu' },
+//   { number: '039', name: 'Jigglypuff' },
+// ];
+
+// const sortedPokemon = pokedex.map(name => <li>{name}</li>);
+// return <ul>{sortedPokemon}</ul>
+
+// function App() {
+//   return <PokemonList {sortedPokemon}/>;
+// }
+
+// export default App;
---
+++ react-lists/my-app/src/PokemonList.tsx
@@ -0,0 +1,12 @@
+export type Pokemon = {
+  number: string;
+  name: string;
+};
+
+// export function PokemonList({ pokedex }: Pokemon[]) {
+//   return (
+//     <ul>
+//       <li>{pokedex}</li>
+//     </ul>
+//   );
+// }
