c9cd44d7ee60bf4bfdf9701c5c0d955354bdeeb0
Ryan Garcia <ryankgarcia22@gmail.com>
2025-04-01 03:12:57 +0000

    Completed react-search.

--- react-search/my-app/src/SearchableList.tsx
+++ react-search/my-app/src/SearchableList.tsx
@@ -1,49 +1,46 @@
+import { useState } from 'react';
 import './SearchableList.css';
-import React, { useState } from 'react';
-
-const quotes = [
-  'Yer a wizard Harry.',
-  "I hope you're pleased with yourselves.",
-  'It does not do well to dwell on dreams and forget to live.',
-  'To the well-organized mind, death is but the next great adventure.',
-  "You're a little scary sometimes, you know that? Brilliant... but scary.",
-  'There will be no foolish wand-waving or silly incantations in this class.',
-  'It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.',
-  'If there is one thing Voldemort cannot understand, it is love.',
-  'As much money and life as you could want!',
-  'The truth. It is a beautiful and terrible thing, and should therefore be treated with great caution.',
-  'There are some things you can’t share without ending up liking each other.',
-  'Ah, music. A magic beyond all we do here!',
-];
-
-// type Props = {
-//   quotes: string[];
-// };
-
-export function SearchableList() {
-  const [lookup, setLookup] = useState('');
-
-  const filterQuotes = quotes.filter((f) =>
-    f.toLowerCase().includes(lookup.toLowerCase())
+type Props = {
+  list: string[];
+};
+export function SearchableList({ list }: Props) {
+  const [value, setValue] = useState('');
+  const items = list.filter((item) =>
+    item.toLowerCase().includes(value.toLowerCase())
   );
-
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setLookup(event.target.value);
-  }
-
   return (
-    <div className="container">
-      <input
-        placeholder="search"
-        type="text"
-        value={lookup}
-        onChange={handleChange}
-      />
-      <ul>
-        {filterQuotes.map((iQ) => (
-          <li>{iQ}</li>
-        ))}
-      </ul>
+    <div className="wrapper">
+      <SearchBar value={value} onCustomChange={setValue} />
+      <Items items={items} />
     </div>
+  );
+}
+type SearchBarProps = {
+  value: string;
+  onCustomChange: (value: string) => void;
+};
+function SearchBar({ value, onCustomChange }: SearchBarProps) {
+  return (
+    <input
+      type="text"
+      className="search"
+      value={value}
+      onChange={(e) => onCustomChange(e.target.value)}
+    />
+  );
+}
+type ItemsProps = {
+  items: string[];
+};
+function Items({ items }: ItemsProps) {
+  if (items.length === 0) {
+    return <ul>No items match the filter.</ul>;
+  }
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
   );
 }
--- react-search/react-search-notes.md
+++ react-search/react-search-notes.md
@@ -2,28 +2,4 @@
 
 ## Notes
 
-All student notes should be written here.
-
-How to write `Code Examples` in markdown
-
-for JS:
-
-```javascript
-const data = 'Howdy';
-```
-
-for HTML:
-
-```html
-<div>
-  <p>This is text content</p>
-</div>
-```
-
-for CSS:
-
-```css
-div {
-  width: 100%;
-}
-```
+\***_You generally want state to live in the component itself_**
