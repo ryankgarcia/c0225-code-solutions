92543ba654e184b8da56679c122bb231cd020d2e
Ryan Garcia <ryankgarcia22@gmail.com>
2025-04-02 19:29:55 +0000

    in progress.

---
+++ react-carousel/my-app/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+// import { FaPlay } from 'react-icons/fa';
+// import { Carousel } from './Carousel';
+
+function App() {
+  return <>{/* <Carousel  /> */}</>;
+}
+
+export default App;
---
+++ react-carousel/my-app/src/Carousel.tsx
@@ -0,0 +1,20 @@
+// export type Image = {
+//   src: string;
+//   alt: string;
+// };
+// const pictures: Image[] = [
+//   { src: '/images/fushiguro.webp', alt: 'Fushi Guro' },
+//   { src: '/images/inumaki.webp', alt: 'Inumaki' },
+//   { src: '/images/itadori.webp', alt: 'Itadori' },
+//   { src: '/images/kugisaki.webp', alt: 'Kugisaki' },
+//   { src: '/images/panda.webp', alt: 'Panda' },
+//   { src: '/images/zen-in.webp', alt: 'Zen-In' },
+// ];
+
+// export function Carousel({ pictures }: Image) {
+//   return (
+//     <div>
+//       <img pictures={pictures} />
+//     </div>
+//   );
+// }
