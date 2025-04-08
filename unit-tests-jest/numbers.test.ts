a79a649942afaa5fdff5f376c42a223a53ad73f6
Ryan Garcia <ryankgarcia22@gmail.com>
2025-04-08 17:54:26 +0000

    in progress...

---
+++ unit-tests-jest/numbers.test.ts
@@ -0,0 +1,9 @@
+// import { evenNumbers, toDollars, divideBy, multiplyBy } from './numbers';
+
+// // 'it' is a synonym for test
+
+// describe('evenNumbers()', () => {
+//   it('return the even numbers', () => {
+//     const result = evenNumbers([1, 4, 5, 10, 0]);
+//   });
+// });
