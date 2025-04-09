e816e2d155a76c560b6fcb8189dbe076ef862195
Ryan Garcia <ryankgarcia22@gmail.com>
2025-04-09 02:16:18 +0000

    Completed string-manipulation-1.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,2 +1,11 @@
 'use strict';
 /* exported getWords */
+function getWords(string) {
+  if (!string) {
+    return [];
+  } else {
+    return string.split(' ');
+  }
+}
+console.log(getWords('Web Developer'));
+console.log(getWords('Ryan Garcia'));
--- string-manipulation-1/src/get-words.ts
+++ string-manipulation-1/src/get-words.ts
@@ -1,1 +1,12 @@
 /* exported getWords */
+
+function getWords(string: string): string[] {
+  if (!string) {
+    return [];
+  } else {
+    return string.split(' ');
+  }
+}
+
+console.log(getWords('Web Developer'));
+console.log(getWords('Ryan Garcia'));
