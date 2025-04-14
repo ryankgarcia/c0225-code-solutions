de16e6bd452783380bfa3c0a3a1246a605b3e6df
Ryan Garcia <ryankgarcia22@gmail.com>
2025-04-14 22:12:17 +0000

    Completed express-error-handling.

---
+++ express-error-handling/lib/error-middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { ClientError } from './client-error.js';
+
+// when there are 4 parameters its error middleware
+// otherwise it is standard middleware
+
+export function errorMiddleware(
+  err: unknown,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- next must be declared for error middleware
+  next: NextFunction
+): void {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({ error: err.message });
+  } else {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred' });
+  }
+}
