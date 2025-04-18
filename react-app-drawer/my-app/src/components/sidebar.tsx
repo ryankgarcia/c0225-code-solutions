33cfbbfa3f0465c1d9dc65979e16e29e27a921f3
Ryan Garcia <ryankgarcia22@gmail.com>
2025-04-18 16:27:24 +0000

    added css still work in progress...

--- react-app-drawer/my-app/src/components/sidebar.tsx
+++ react-app-drawer/my-app/src/components/sidebar.tsx
@@ -1,29 +1,31 @@
-// import { FaBars } from 'react-icons/fa';
-// import { BsShop } from 'react-icons/bs';
-// import { GiEagleEmblem } from 'react-icons/gi';
-// import { Link } from 'react-router-dom';
-// import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { BsShop } from 'react-icons/bs';
+import { GiEagleEmblem } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-// export function Sidebar() {
-//   const [ isOpen, setIsOpen ] = useState(false);
+export function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
 
-//   function handleOpen() {
-//     setIsOpen(handleOpen === true ? className='hamburger-open');
-//   }
-//   return (
-//     <div onClick={handleOpen} >
-//       <div className="icon-colors">
-//         <FaBars size={30} />
-//       </div>
-//       <h2 className="hs-text">Hylian Shopping</h2>
-//       <Link to="about" className="icon-colors">
-//         <GiEagleEmblem size={30} className="icon-colors" />
-//         <span>About</span>
-//       </Link>
-//       <Link to="/" className="icon-colors">
-//         <BsShop size={30} />
-//         <span>Catalog</span>
-//       </Link>
-//     </div>
-//   );
-// }
+  function handleOpen() {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <div
+      className={isOpen ? 'hamburger-open' : 'hamburger-closed'}
+      onClick={handleOpen}>
+      <div className="icon-colors">
+        <FaBars size={30} />
+      </div>
+      {isOpen ? <h2 className="hs-text">Hylian Shopping</h2> : ''}
+      <Link to="about" className="icon-colors">
+        <GiEagleEmblem size={30} className="icon-colors" />
+        {isOpen ? <span>About</span> : ''}
+      </Link>
+      <Link to="/" className="icon-colors">
+        <BsShop size={30} />
+        {isOpen ? <span>Catalog</span> : ' '}
+      </Link>
+    </div>
+  );
+}
