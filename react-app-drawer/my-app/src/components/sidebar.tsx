9654e1bba21042bc5a507584da242527f8b8f076
Ryan Garcia <ryankgarcia22@gmail.com>
2025-04-18 04:43:59 +0000

    in progress..

---
+++ react-app-drawer/my-app/src/components/sidebar.tsx
@@ -0,0 +1,29 @@
+// import { FaBars } from 'react-icons/fa';
+// import { BsShop } from 'react-icons/bs';
+// import { GiEagleEmblem } from 'react-icons/gi';
+// import { Link } from 'react-router-dom';
+// import { useState } from 'react';
+
+// export function Sidebar() {
+//   const [ isOpen, setIsOpen ] = useState(false);
+
+//   function handleOpen() {
+//     setIsOpen(handleOpen === true ? className='hamburger-open');
+//   }
+//   return (
+//     <div onClick={handleOpen} >
+//       <div className="icon-colors">
+//         <FaBars size={30} />
+//       </div>
+//       <h2 className="hs-text">Hylian Shopping</h2>
+//       <Link to="about" className="icon-colors">
+//         <GiEagleEmblem size={30} className="icon-colors" />
+//         <span>About</span>
+//       </Link>
+//       <Link to="/" className="icon-colors">
+//         <BsShop size={30} />
+//         <span>Catalog</span>
+//       </Link>
+//     </div>
+//   );
+// }
