23e048fbdb98f15d2182ab10ad583037cd402bd3
Ryan Garcia <ryankgarcia22@gmail.com>
2025-05-13 02:18:28 +0000

    completed array-manipulation-2.

--- array-manipulation-2/src/chunk.ts
+++ array-manipulation-2/src/chunk.ts
@@ -1,1 +1,16 @@
 /* exported chunk */
+
+function chunk(arr: number[], size: number): number[][] {
+  const result: number[][] = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    const chunk: number[] = [];
+    for (let j = i; j < i + size && j < arr.length; j++) {
+      chunk.push(arr[j]);
+    }
+    result.push(chunk);
+  }
+  return result;
+}
+
+console.log('chunk arr:', chunk([1, 2, 3, 4, 5, 6, 7, 8, 9], 2));
