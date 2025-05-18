325992bbd5baabef63497b013fbf4f5ca0a1b0de
Ryan Garcia <ryankgarcia22@gmail.com>
2025-05-18 06:14:15 +0000

    in progress title case...

--- practice-exercises/01-basic-challenges-1/06-title-case/title-case.ts
+++ practice-exercises/01-basic-challenges-1/06-title-case/title-case.ts
@@ -1,3 +1,10 @@
 export function titleCase(str: string): string {
-  return '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[0]) {
+      str[0].toUpperCase();
+    } else {
+      str[i].toLowerCase();
+    }
+  }
+  return str;
 }
