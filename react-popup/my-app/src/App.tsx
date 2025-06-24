f547af8fe395990635f65eac705fced10595f1bb
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-24 23:44:16 +0000

    positioned the popup in the middle of the page and styled it so when clicked outside of the popup, popup closes.

--- react-popup/my-app/src/App.tsx
+++ react-popup/my-app/src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Popup } from './Popup';
 import './App.css';
 
 function App() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   function togglePopup() {
     setIsPopupOpen((prev) => !prev);
@@ -11,12 +12,35 @@ function App() {
 
   return (
     <>
-      <button onClick={togglePopup}>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <button ref={buttonRef} onClick={togglePopup}>
         {isPopupOpen ? 'Close Popup' : 'Open Popup'}
       </button>
       {isPopupOpen && (
-        <Popup isPopupOpen={isPopupOpen} setIsPopupOpen={setIsPopupOpen} />
+        <Popup
+          isPopupOpen={isPopupOpen}
+          setIsPopupOpen={setIsPopupOpen}
+          positionTo={buttonRef.current}
+          onClose={() => setIsPopupOpen(false)}
+        />
       )}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
     </>
   );
 }
--- react-popup/my-app/src/Popup.tsx
+++ react-popup/my-app/src/Popup.tsx
@@ -1,27 +1,63 @@
+import { createPortal } from 'react-dom';
 import './Popup.css';
 
 type Props = {
   isPopupOpen: boolean;
   setIsPopupOpen: (open: boolean) => void;
+  positionTo?: HTMLElement | null;
+  onClose?: () => void;
 };
 
-export function Popup({ isPopupOpen, setIsPopupOpen }: Props) {
+export function Popup({
+  isPopupOpen,
+  setIsPopupOpen,
+  positionTo,
+  onClose,
+}: Props) {
   if (!isPopupOpen) return null;
-  return (
-    <div>
-      <ul
-        style={{ listStyleType: 'none' }}
-        className="shaded-border"
-        onClick={() => setIsPopupOpen(false)}>
-        <li>Dog</li>
-        <li>Cat</li>
-        <li>Giraffe</li>
-        <li>Goat</li>
-        <li>Duck</li>
-        <li>Monkey</li>
-        <li>Bull</li>
-        <li>Pig</li>
-      </ul>
-    </div>
+
+  let top = '50%';
+  let left = '50%';
+
+  if (positionTo) {
+    const rect = positionTo.getBoundingClientRect();
+    top = `${rect.bottom + 8}px`;
+    left = `${rect.left}px`;
+  }
+  return createPortal(
+    <>
+      <div
+        onClick={onClose}
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          backgroundColor: 'black',
+          width: '100vw',
+          height: '100vh',
+          opacity: '.5',
+          zIndex: 999,
+          pointerEvents: 'auto',
+        }}>
+        <div style={{ position: 'absolute', top, left, zIndex: 1000 }}>
+          <div>
+            <ul
+              style={{ listStyleType: 'none' }}
+              className="shaded-border"
+              onClick={() => setIsPopupOpen(false)}>
+              <li>Dog</li>
+              <li>Cat</li>
+              <li>Giraffe</li>
+              <li>Goat</li>
+              <li>Duck</li>
+              <li>Monkey</li>
+              <li>Bull</li>
+              <li>Pig</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>,
+    document.body
   );
 }
