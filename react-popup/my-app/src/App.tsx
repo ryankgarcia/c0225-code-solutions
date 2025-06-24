22dfc62b556950c6863fae05d84470f7e2200d10
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-24 21:36:33 +0000

    completed steps 1-4.

---
+++ react-popup/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { Popup } from './Popup';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <button>Pop up!</button>
+      <Popup />
+    </>
+  );
+}
+
+export default App;
---
+++ react-popup/my-app/src/Popup.tsx
@@ -0,0 +1,16 @@
+export function Popup() {
+  return (
+    <div>
+      <ul>
+        <li>Dog</li>
+        <li>Cat</li>
+        <li>Giraffe</li>
+        <li>Goat</li>
+        <li>Duck</li>
+        <li>Monkey</li>
+        <li>Bull</li>
+        <li>Pig</li>
+      </ul>
+    </div>
+  );
+}
