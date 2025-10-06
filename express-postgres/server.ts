5e6be7fb3fd3ab87ea980fe2e6124b02ba56e161
Ryan Garcia <ryankgarcia22@gmail.com>
2025-10-06 19:26:09 +0000

    reworking the exercise for practicing server codes.

--- express-postgres/server.ts
+++ express-postgres/server.ts
@@ -11,8 +11,9 @@ const db = new pg.Pool({
 
 const app = express();
 
-// GET /api/films returns all films in the table, ordered from highest replacement cost to lowest
+// GET /api/films => returns all films in the table, ordered from highest replacement cost to lowest
 
+// get req is simple. first, do the sql query. second, await db.query(sql). third, res.send(result.rows)
 app.get('/api/films', async (req, res, next) => {
   try {
     const sql = `
@@ -27,21 +28,20 @@ app.get('/api/films', async (req, res, next) => {
   }
 });
 
-// GET /api/films/:filmId returns a single film, based on filmId. Return a 404 not found if the filmId is not in the database
+// GET /api/films/:filmId => returns a single film, based on filmId. Return a 404 Not found if the filmId is not in the DB
 
 app.get('/api/films/:filmId', async (req, res, next) => {
   try {
     const { filmId } = req.params;
     if (!Number(filmId)) {
-      throw new ClientError(400, 'filmId must be a positive number');
+      throw new ClientError(400, 'filmId must be a positive integer');
     }
     const sql = `
     select *
     from "films"
     where "filmId" = $1
     `;
-    const params = [filmId];
-    const result = await db.query(sql, params);
+    const result = await db.query(sql, [filmId]);
     const film = result.rows[0];
     if (!film) {
       throw new ClientError(404, `film ${filmId} not found`);
@@ -52,10 +52,8 @@ app.get('/api/films/:filmId', async (req, res, next) => {
   }
 });
 
-// PUT /api/films/:filmId updates a single film title, based on filmId. pass title as a query parameter. return the updated film.
-// return a 404 not found if the filmId is not in the database
-
-// do not put template literals in your SQL queries. doing so will subject you to injection attacks
+// PUT /api/films/:filmId => updates a single film title, based on filmId. Pass title as a query parameter.
+// return the updated film. return 404 not found if the filmId is not in the DB
 
 app.put('/api/films/:filmId', async (req, res, next) => {
   try {
@@ -68,11 +66,11 @@ app.put('/api/films/:filmId', async (req, res, next) => {
       throw new ClientError(400, 'title is required');
     }
     const sql = `
-    update "films"
-    set "title" = $2
-    where "filmId" = $1
-    returning *;
-    `;
+   update "films"
+   set "title" = $2
+   where "filmId" = $1
+   returning *;
+   `;
     const result = await db.query(sql, [filmId, title]);
     const film = result.rows[0];
     if (!film) {
